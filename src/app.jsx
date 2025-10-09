8ce32c88d300f46fcde9e2b74653457813242ee2
Luke Olsen <lukeolsen77@gmail.com>
2025-10-08 23:21:08 -0600

    updated code to be able to see the changes on vite

--- src/app.jsx
+++ src/app.jsx
@@ -5,56 +5,56 @@ import './app.css';
 import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
 import { Home } from './home/home';
 import { Login } from './login/login';
-import { Task } from './tasks/tasks';
+import { Tasks } from './tasks/tasks';
 
 export default function App() {
-  return (
-    <div>
+    return (
         <BrowserRouter>
-            <header className="container-fluid">
-                <nav className="navbar navbar-expand-lg custom-navbar">
-                    <div className="container-fluid">
-                        <a className="navbar-brand" href="#">
-                            <img alt="tasktrackerappicon" src="tasktrackerappicon.png" width="40" height="40" />
-                            Task Tracker App
-                        </a>
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-                        <div className="collapse navbar-collapse" id="navbarNav">
-                            <ul className="navbar-nav ms-auto">
-                                <li className="nav-item">
-                                    <NavLink className="nav-link" to="/home">Home</NavLink>
-                                </li>
-                                <li className="nav-item">
-                                    <NavLink className="nav-link" to="/tasks">All Tasks</NavLink>
-                                </li>
-                                <li className="nav-item">
-                                    <NavLink className="nav-link" to="/login">Logout</NavLink>
-                                </li>
-                            </ul>
+            <div>
+                <header className="container-fluid">
+                    <nav className="navbar navbar-expand-lg custom-navbar">
+                        <div className="container-fluid">
+                            <a className="navbar-brand" href="#">
+                                <img alt="tasktrackerappicon" src="tasktrackerappicon.png" width="40" height="40" />
+                                Task Tracker App
+                            </a>
+                            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                                <span className="navbar-toggler-icon"></span>
+                            </button>
+                            <div className="collapse navbar-collapse" id="navbarNav">
+                                <ul className="navbar-nav ms-auto">
+                                    <li className="nav-item">
+                                        <NavLink className="nav-link" to="/home">Home</NavLink>
+                                    </li>
+                                    <li className="nav-item">
+                                        <NavLink className="nav-link" to="/tasks">All Tasks</NavLink>
+                                    </li>
+                                    <li className="nav-item">
+                                        <NavLink className="nav-link" to="/login">Logout</NavLink>
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
+                    </nav>
+                </header>
+
+                <Routes>
+                    <Route path="/home" element={<Home />} />
+                    <Route path="/tasks" element={<Tasks />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+
+                <footer>
+                    <div className="footer-content">
+                        <span className="text-reset">Author Name: Luke Olsen</span>
+                        <br />
+                        <a href="https://github.com/bruhmomentooo/startup.git">GitHub</a>
                     </div>
-                </nav>
-            </header>
-        </BrowserRouter>
-
-        <Routes>
-            <Route path="/home" element={<Home />} />
-            <Route path="/tasks" element={<Task />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="*" element={<NotFound />} />
-        </Routes>
-
-        <footer>
-            <div className="footer-content">
-                <span className="text-reset">Author Name: Luke Olsen</span>
-                <br />
-                <a href="https://github.com/bruhmomentooo/startup.git">GitHub</a>
+                </footer>
             </div>
-        </footer>
-    </div>
-  );
+        </BrowserRouter>
+    );
 }
 
 function NotFound() {
--- src/tasks/tasks.jsx
+++ src/tasks/tasks.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export function Home() {
+export function Tasks() {
   return (
     <main>
       
