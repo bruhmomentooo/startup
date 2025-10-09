239a716786d165bdbf9c7747340c33a31771ea64
Luke Olsen <lukeolsen77@gmail.com>
2025-10-08 23:06:14 -0600

    added some basic code to the new files and added more imports to app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -2,35 +2,42 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Home } from './home/home';
+import { Login } from './login/login';
+import { Task } from './tasks/task';
+
 export default function App() {
   return (
     <div>
-        <header className="container-fluid">
-            <nav className="navbar navbar-expand-lg custom-navbar">
-                <div className="container-fluid">
-                    <a className="navbar-brand" href="#">
-                        <img alt="tasktrackerappicon" src="tasktrackerappicon.png" width="40" height="40" />
-                        Task Tracker App
-                    </a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarNav">
-                        <ul className="navbar-nav ms-auto">
-                            <li className="nav-item">
-                                <a className="nav-link" href="home.html">Home</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="tasks.html">All Tasks</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="index.html">Logout</a>
-                            </li>
-                        </ul>
+        <BrowserRouter>
+            <header className="container-fluid">
+                <nav className="navbar navbar-expand-lg custom-navbar">
+                    <div className="container-fluid">
+                        <a className="navbar-brand" href="#">
+                            <img alt="tasktrackerappicon" src="tasktrackerappicon.png" width="40" height="40" />
+                            Task Tracker App
+                        </a>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div className="collapse navbar-collapse" id="navbarNav">
+                            <ul className="navbar-nav ms-auto">
+                                <li className="nav-item">
+                                    <a className="nav-link" href="home.html">Home</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="tasks.html">All Tasks</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="index.html">Logout</a>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
-                </div>
-            </nav>
-        </header>
+                </nav>
+            </header>
+        </BrowserRouter>
 
         <main>App components go here</main>
 
