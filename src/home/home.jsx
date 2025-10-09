c83c3cb94a116745e511c238f69669650c14c667
Luke Olsen <lukeolsen77@gmail.com>
2025-10-08 23:34:03 -0600

    moved files and filled in code

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -1,9 +1,86 @@
 import React from 'react';
+import './home-styles.css';
 
 export function Home() {
   return (
     <main>
-      
+      <div class="content">
+                <div id="siteSidebar" class="sidebar">
+                    <button type="get">Create</button>
+                    <button type="get">Select/Edit</button>
+                    <button type="get">Add to Google Calendar</button>
+                    <button type="get">Notifications</button>
+
+                </div>
+
+                <div class="task_container" id="recurringTasks">
+                    <h3>Recurring Tasks</h3>
+                    <div class="task">
+                            <input class="form-check-input" type="checkbox" id="recurring1" />
+                            <div class="task-details">
+                                <label for="recurring1">Task 1</label>
+                                <p>Description for Task 1</p>
+                                <p><i>Last completed: 08-14-2023</i></p>
+                            </div>
+                            <button type="button" onclick="viewTask('Task 1', 'Details about Task 1')">View</button>
+                            <button type="button" onclick="editTask('Task 1')">Edit</button>
+                    </div>
+                    <div class="task">
+                            <input class="form-check-input" type="checkbox" id="recurring2" />
+                            <div class="task-details">
+                                <label for="recurring2">Task 2</label>
+                                <p>Description for Task 2</p>
+                                <p><i>Last completed: 9-30-2024</i></p>
+                            </div>
+                            <button type="button" onclick="viewTask('Task 2', 'Details about Task 2')">View</button>
+                            <button type="button" onclick="editTask('Task 2')">Edit</button>
+                    </div>
+                    <div class="task">
+                            <input class="form-check-input" type="checkbox" id="recurring3" />
+                            <div class="task-details">
+                                <label for="recurring3">Task 3</label>
+                                <p>Description for Task 3</p>
+                                <p><i>Last completed: 02-01-2025</i></p>
+                            </div>
+                            <button type="button" onclick="viewTask('Task 3', 'Details about Task 3')">View</button>
+                            <button type="button" onclick="editTask('Task 3')">Edit</button>
+                    </div>
+                </div>
+                
+                <div class="task_container" id="allTasks">
+                    <h3>All Tasks</h3>
+                    <div class="task">
+                        <input class="form-check-input" type="checkbox" id="all1" />
+                        <div class="task-details">
+                            <label for="all1">Task 1</label>
+                            <p>Description for Task 1</p>
+                            <p><i>Last completed: 08-14-2023</i></p>
+                        </div>
+                        <button type="button" onclick="viewTask('Task 1', 'Details about Task 1')">View</button>
+                        <button type="button" onclick="editTask('Task 1')">Edit</button>
+                    </div>
+                    <div class="task">
+                        <input class="form-check-input" type="checkbox" id="all2" />
+                        <div class="task-details">
+                            <label for="all2">Task 2</label>
+                            <p>Description for Task 2</p>
+                            <p><i>Last completed: 9-30-2024</i></p>
+                        </div>
+                        <button type="button" onclick="viewTask('Task 2', 'Details about Task 2')">View</button>
+                        <button type="button" onclick="editTask('Task 2')">Edit</button>
+                    </div>
+                    <div class="task">
+                        <input class="form-check-input" type="checkbox" id="all3" />
+                        <div class="task-details">
+                            <label for="all3">Task 3</label>
+                            <p>Description for Task 3</p>
+                            <p><i>Last completed: 02-01-2025</i></p>
+                        </div>
+                        <button type="button" onclick="viewTask('Task 3', 'Details about Task 3')">View</button>
+                        <button type="button" onclick="editTask('Task 3')">Edit</button>
+                    </div>
+                </div>
+            </div>
     </main>
   );
 }
--- src/login/login.jsx
+++ src/login/login.jsx
@@ -3,7 +3,24 @@ import React from 'react';
 export function Login() {
   return (
     <main>
-        
-    </main>
+            <div className="welcome_container">
+                <h1>Welcome!</h1>
+                <p style={{textAlign: "center"}}>Please log in or create an account to begin your task management.</p>
+                <form method="get" action="home.html">
+                    <div>
+                        <label htmlFor="email"><b>Email:</b></label>
+                        <input type="text" id="email" name="email" required placeholder="example@gmail.com" />
+                    </div>
+                    <div>
+                        <p><label htmlFor="password"><b>Password:</b></label>
+                        <input type="password" id="password" name="password" required placeholder="password" /></p>
+                    </div>
+                    <div>
+                        <p><button type="submit">Login</button></p>
+                        <p><button type="submit">Create Account</button></p>
+                    </div>
+                </form>
+            </div>
+        </main>
   );
 }
